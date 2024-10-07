59ac2c7f0c73785c99267c16cc3acaf7d98e37d2
Jitesh pal <105933945+Jitesh2604@users.noreply.github.com>
2024-10-07 17:40:13 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,12 @@ const connectDB = async() => {
     try {
         await mongoose.connect(url,
             {
-                family:4
+                family:4,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+                socketTimeoutMS: 45000, // 45 seconds for socket inactivity timeout
+            }
             });
         console.log(`MongoDB Connected Successfully`);
     } catch (err) {
