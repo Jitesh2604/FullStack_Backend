b7ee8c78d98f998e4958f9982b56fd8bddcf7421
Jitesh <jiteshpal2604@gmail.com>
2024-10-07 16:16:36 +0530

    added

--- server/server.js
+++ server/server.js
@@ -15,6 +15,13 @@ app.use(cors({
 
 app.use(express.json())
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("connected")
 })
